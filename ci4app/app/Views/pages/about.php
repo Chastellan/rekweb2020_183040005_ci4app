98346af2dd11bf56ff6a3d789cfd98b4e009323b
Chastellan <72085118+Chastellan@users.noreply.github.com>
2020-10-21 23:09:47 +0700

    Tugas CodeIgniter (Bagian 1)

---
+++ ci4app/app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eum ea earum enim molestiae nulla error magnam, doloremque officiis mollitia illum blanditiis fugit incidunt ducimus dicta cupiditate sed vel quaerat.</p>
+        </div>
+    </div>
+</div>
